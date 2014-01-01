04d72cbbf4635bc836b2c4f14187342dfe1e2c26
Artyom Trityak <art.trityak@gmail.com>
2014-01-01 16:59:27 +0200

    Travis start

--- tests/spec/test.js
+++ tests/spec/test.js
@@ -3,7 +3,7 @@ var expect = chai.expect;
 describe('Backbone.Controller', function(){
   
   it('should return -1 when not present', function(){
-    
+    expect(1).to.be.equal(1);
   });
-  
+
 });
